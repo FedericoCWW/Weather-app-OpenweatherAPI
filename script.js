d701158149ee0f548b5fec17afee043982d67d6c
Federico <133919977+FedericoCWW@users.noreply.github.com>
2023-08-25 20:06:46 -0300

    v4

--- script.js
+++ script.js
@@ -5,53 +5,56 @@ const searchBtn = document.querySelector(".display button");
 const weatherIcon = document.getElementById("weather-icon")
 async function CheckW(city){
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
-    let data = await response.json();
 
-    console.log(data)
-    console.log(weatherIcon.innerHTML)
-
-    document.querySelector(".city").innerHTML = data.name + ", " + data.sys.country;
-    document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    if (response.status == 404){
+        alert("Ciudad Invalida")
+    }else{
+        let data = await response.json();
     
-    switch(data.weather[0].description){
-        case "clear sky":
-            weatherIcon.className = "bi bi-sun-fill";
-            break;
-        case "few clouds":
-            weatherIcon.className = "bi bi-cloud-sun-fill";
-            break;
-        case "scattered clouds":
-            weatherIcon.className = "bi bi-cloudy";
-            break;
-        case "broken clouds":
-            weatherIcon.className = "bi bi-clouds";
-            break;
-        case "shower rain":
-            weatherIcon.className = "bi bi-cloud-rain-fill";
-            break;
-        case "rain":
-            weatherIcon.className = "bi bi-cloud-rain-heavy-fill";
-            break;
-        case "thunderstorm":
-            weatherIcon.className = "bi bi-cloud-lightning-rain-fill";
-            break;
-        case "snow":
-            weatherIcon.className = "bi bi-snow";
-            break;
-        case "mist":
-            weatherIcon.className = "bi bi-cloud-fog2-fill";
-            break;
+        console.log(data)
+        console.log(weatherIcon.innerHTML)
     
+        document.querySelector(".city").innerHTML = data.name + ", " + data.sys.country;
+        document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        
+        switch(data.weather[0].description){
+            case "clear sky":
+                weatherIcon.className = "bi bi-sun-fill";
+                break;
+            case "few clouds":
+                weatherIcon.className = "bi bi-cloud-sun-fill";
+                break;
+            case "scattered clouds":
+                weatherIcon.className = "bi bi-cloudy";
+                break;
+            case "broken clouds":
+                weatherIcon.className = "bi bi-clouds";
+                break;
+            case "shower rain":
+                weatherIcon.className = "bi bi-cloud-rain-fill";
+                break;
+            case "rain":
+                weatherIcon.className = "bi bi-cloud-rain-heavy-fill";
+                break;
+            case "thunderstorm":
+                weatherIcon.className = "bi bi-cloud-lightning-rain-fill";
+                break;
+            case "snow":
+                weatherIcon.className = "bi bi-snow";
+                break;
+            case "mist":
+                weatherIcon.className = "bi bi-cloud-fog2-fill";
+                break;
+        
+        }
+        document.querySelector(".weather").style.display = "block"
     }
-
 };
 
 searchBtn.addEventListener("click", () => {
     CheckW(search.value);
 });
-CheckW()
-
 //console.log(apiKey);
 
