249ce4930d3ebf624d92b497484f82846f4e6736
Federico <133919977+FedericoCWW@users.noreply.github.com>
2023-08-25 22:22:17 -0300

    v4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 const search = document.querySelector(".display input");
 const searchBtn = document.querySelector(".display button");
 const weatherIcon = document.getElementById("weather-icon")
+const weatherDesc = document.getElementById("weather-desc")
 async function CheckW(city){
     const response = await fetch(apiUrl + city +`&appid=${apiKey}`);
 
@@ -22,30 +23,39 @@ async function CheckW(city){
         switch(data.weather[0].description){
             case "clear sky":
                 weatherIcon.className = "bi bi-sun-fill";
+                weatherDesc.innerHTML = "Cielo despejado";
                 break;
             case "few clouds":
                 weatherIcon.className = "bi bi-cloud-sun-fill";
+                weatherDesc.innerHTML = "Pocas Nubes";
                 break;
             case "scattered clouds":
                 weatherIcon.className = "bi bi-cloudy";
+                weatherDesc.innerHTML = "Nubes dispersas";
                 break;
             case "broken clouds":
                 weatherIcon.className = "bi bi-clouds";
+                weatherDesc.innerHTML = "Nubes discontinuas";
                 break;
             case "shower rain":
                 weatherIcon.className = "bi bi-cloud-rain-fill";
+                weatherDesc.innerHTML = "Lluvia ligera";
                 break;
             case "rain":
                 weatherIcon.className = "bi bi-cloud-rain-heavy-fill";
+                weatherDesc.innerHTML = "Lluvia";
                 break;
             case "thunderstorm":
                 weatherIcon.className = "bi bi-cloud-lightning-rain-fill";
+                weatherDesc.innerHTML = "Lluvia tormentosa";
                 break;
             case "snow":
                 weatherIcon.className = "bi bi-snow";
+                weatherDesc.innerHTML = "Nieve";
                 break;
             case "mist":
                 weatherIcon.className = "bi bi-cloud-fog2-fill";
+                weatherDesc.innerHTML = "Niebla";
                 break;
         
         }
